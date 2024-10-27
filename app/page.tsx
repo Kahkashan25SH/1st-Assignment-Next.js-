57c2be97bc8111cdd3d38fb173e9a432459e8e21
Kahkashan Ahmed Ali <162057212+Kahkashan25SH@users.noreply.github.com>
2024-10-28 00:33:34 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,10 @@
 import Link from 'next/link';
 import React from 'react'
-import Navbar from './component/Navbar'
-import Hero from './component/Hero'
-import Footer from './component/Footer'
 
-// Home component
 function HomePage(){
   return (
-    <div>
-        <Navbar />
-        <Hero />
-        <Footer />
-          
-    </div>
+    <h1 >Wellcome to the Home Page </h1>
   )
-}
+    
 export default HomePage;
 
-// px (pixel per inch)
-// % percentage (percentage of the body)
-// png, jpg, jpeg, svg, webp.
-// bg-coverd 
-// theme coustamization color "tailwind.config.ts" mein ja k color k object mein 
-// rm -rf.next // remove, ropefully, command jub  error aye.

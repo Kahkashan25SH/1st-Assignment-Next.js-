b05000578125f6c8f3af6a3c935bd90688e9541f
Kahkashan Ahmed Ali <162057212+Kahkashan25SH@users.noreply.github.com>
2024-11-01 13:34:27 +0500

    page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,15 +7,6 @@ const About: React.FC = () => {
       <p>
         As a web developer, I specialize in building responsive, user-friendly websites that help businesses and individuals establish their online presence. <br /> My goal is to create websites that are not only visually appealing but also functional and accessible to all users.
       </p>
-
-       {/* Add Home Button */}
-
-       <div className="mt-10">
-          <Link href="/">
-          <button className="bg-white text-fuchsia-600 px-4 py-2 rounded-full hover:bg-fuchsia-900 hover:text-white transition">
-            Go to Home Page</button>
-          </Link>
-       </div>
     </div>
     )
   };

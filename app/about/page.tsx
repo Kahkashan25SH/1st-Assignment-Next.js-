d7f1e7a03fbee5bc68510e50173107b761932b39
Kahkashan Ahmed Ali <162057212+Kahkashan25SH@users.noreply.github.com>
2024-11-01 13:35:17 +0500

    page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,8 +2,8 @@ import Link from "next/link"
 
 const About: React.FC = () => {
     return (
-    <div className="bg-fuchsia-600 text-white pt-10 pb-10 text-center m-20 rounded-md">
-        <h1 className="font-extrabold"> About Us:</h1>
+    <div>
+        <h1> About Us:</h1>
       <p>
         As a web developer, I specialize in building responsive, user-friendly websites that help businesses and individuals establish their online presence. <br /> My goal is to create websites that are not only visually appealing but also functional and accessible to all users.
       </p>
